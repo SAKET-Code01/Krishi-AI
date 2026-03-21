c7562592a2a56232a3960db99040d775c159e92e
adityakumardutta <adityakumardutta22@gmail.com>
2026-03-21 19:05:31 +0530

    changed font size

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,13 +15,13 @@ export default {
     },
     extend: {
       fontSize: {
-        xs: ['1rem', { lineHeight: '1.5rem' }],
-        sm: ['1.125rem', { lineHeight: '1.75rem' }],
-        base: ['1.25rem', { lineHeight: '1.75rem' }],
-        lg: ['1.5rem', { lineHeight: '2rem' }],
-        xl: ['1.875rem', { lineHeight: '2.25rem' }],
-        '2xl': ['2.25rem', { lineHeight: '2.5rem' }],
-        '3xl': ['3rem', { lineHeight: '3rem' }],
+        xs: ['0.875rem', { lineHeight: '1.25rem' }],
+        sm: ['1rem', { lineHeight: '1.5rem' }],
+        base: ['1.125rem', { lineHeight: '1.75rem' }],
+        lg: ['1.25rem', { lineHeight: '1.75rem' }],
+        xl: ['1.5rem', { lineHeight: '2rem' }],
+        '2xl': ['1.875rem', { lineHeight: '2.25rem' }],
+        '3xl': ['2.25rem', { lineHeight: '2.5rem' }],
       },
       fontFamily: {
         display: ["Plus Jakarta Sans", "sans-serif"],
